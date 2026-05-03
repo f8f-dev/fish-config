291f5e946c0e421ac87e327ff6184dec4038053a
Noct <225653989+f8f-dev@users.noreply.github.com>
2026-05-04 05:10:35 +0900

    upd

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ```sh
 git clone https://github.com/f8f-dev/fish-config.git ~/.config/fish/
 ```
+add bitwarden secret manager token into fish
+```sh
+echo "your-access-token" > ~/.bws-token
+chmod 600 ~/.bws-token
+```
--- config.fish
+++ config.fish
@@ -1,4 +1,7 @@
 if status is-interactive
+  # Commands to run in interactive sessions can go here
   set -gx SSH_AUTH_SOCK "$HOME/Library/Containers/com.bitwarden.desktop/Data/.bitwarden-ssh-agent.sock"
-# Commands to run in interactive sessions can go here
+  if test -f ~/.bws-token
+    set -gx BWS_ACCESS_TOKEN (cat ~/.bws-token)
+  end
 end
